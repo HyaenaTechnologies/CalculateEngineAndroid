874a6b7733a461574d91c61e133f9c4092e08c6d
Abdhilahi R. Wabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2024-05-19 22:35:30 -0600

    Add Exponents

---
+++ lib/src/main/kotlin/dev/hyaena_technologies/calculate_engine/logic/Radius.kt
@@ -0,0 +1,9 @@
+package dev.hyaena_technologies.calculate_engine.logic
+
+// Radius of Objects
+class Radius {
+    // Radius of a Circle
+    fun circle(diameter: Double): Double {
+        return diameter / 2
+    }
+}
