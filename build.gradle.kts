414d715b46dfa9562e73404a80b9f101e84221aa
Abdhilahi R. Wabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2024-06-15 02:28:11 -0600

    Update Kotlin Version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     val com_android = "8.5.0"
-    val org_jetbrains_kotlin = "1.9.24"
+    val org_jetbrains_kotlin = "2.0.0"
 
     id("com.android.application") version "$com_android" apply false
     id("com.android.library") version "$com_android" apply false
@@ -10,4 +10,5 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "$org_jetbrains_kotlin" apply false
     id("org.jetbrains.kotlin.js") version "$org_jetbrains_kotlin" apply false
     id("org.jetbrains.kotlin.multiplatform") version "$org_jetbrains_kotlin" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "$org_jetbrains_kotlin" apply false
 }
