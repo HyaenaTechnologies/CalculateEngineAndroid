9f7bf335861394dbbad3697a020191ed810ed2b7
Abdhilahi R. Wabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2024-06-26 06:18:56 -0600

    Fix Returns

--- lib/src/main/kotlin/dev/hyaena_technologies/calculate_engine/logic/math/conversion/CentimeterConversion.kt
+++ lib/src/main/kotlin/dev/hyaena_technologies/calculate_engine/logic/math/conversion/CentimeterConversion.kt
@@ -4,84 +4,70 @@ package dev.hyaena_technologies.calculate_engine.logic.math.conversion
 
 // Decameters to Centimeters
 fun DecametersToCentimeters(decameters: Double): Double {
-    val centimeters: Double = decameters * 1000
-    return centimeters
+    return decameters * 1000
 }
 
 // Decimeters to Centimeters
 fun DecimetersToCentimeters(decimeters: Double): Double {
-    val centimeters: Double = decimeters * 10
-    return centimeters
+    return decimeters * 10
 }
 
 // Feet to Centimeters
 fun FeetToCentimeters(feet: Double): Double {
-    val centimeters: Double = feet * 30.48
-    return centimeters
+    return feet * 30.48
 }
 
 // Hectometers to Centimeters
 fun HectometersToCentimeters(hectometers: Double): Double {
-    val centimeters: Double = hectometers * 10000
-    return centimeters
+    return hectometers * 10000
 }
 
 // Inches to Centimeters
 fun InchesToCentimeters(inches: Double): Double {
-    val centimeters: Double = inches * 2.54
-    return centimeters
+    return inches * 2.54
 }
 
 // Kilometers to Centimeters
 fun KilometersToCentimeters(kilometers: Double): Double {
-    val centimeters: Double = kilometers * 100000
-    return centimeters
+    return kilometers * 100000
 }
 
 // Meters to Centimeters
 fun MetersToCentimeters(meters: Double): Double {
-    val centimeters: Double = meters * 100
-    return centimeters
+    return meters * 100
 }
 
 // Micrometers to Centimeters
 fun MicrometersToCentimeters(micrometers: Double): Double {
-    val centimeters: Double = micrometers * -10000
-    return centimeters
+    return micrometers * -10000
 }
 
 // Miles to Centimeters
 fun MilesToCentimeters(miles: Double): Double {
-    val centimeters: Double = miles * (1.609344 * 100000)
-    return centimeters
+    return miles * (1.609344 * 100000)
 }
 
 // Millimeters to Centimeters
 fun MillimetersToCentimeters(millimeters: Double): Double {
-    val centimeters: Double = millimeters * 0.1
-    return centimeters
+    return millimeters * 0.1
 }
 
 // Nanometers to Centimeters
 fun NanometersToCentimeters(nanometers: Double): Double {
-    val centimeters: Double = nanometers * -10000000
-    return centimeters
+    return nanometers * -10000000
 }
 
 // Nautical Miles to Centimeters
 fun NauticalMilesToCentimeters(nauticalMiles: Double): Double {
-    val centimeters: Double = nauticalMiles * (1.852001 * 100000)
-    return centimeters
+    return nauticalMiles * (1.852001 * 100000)
 }
 
 // Picometers to Centimeters
 fun PicometersToCentimeters(picometers: Double): Double {
-    val centimeters: Double = picometers * -10000000000
-    return centimeters
+    return picometers * -10000000000
 }
 
 // Yards to Centimeters
 fun YardsToCentimeters(yards: Double): Double {
-    val centimeters: Double = yards * 91.44
-    return centimeters
+    return yards * 91.44
 }
