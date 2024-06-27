38916959c3ccd86857163e308c136ed88c5544cc
Abdhilahi R. Wabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2024-06-27 03:21:13 -0600

    Fix Syntax

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
-    val com_android = "8.5.0"
-    val org_jetbrains_kotlin = "2.0.0"
+    val comAndroid = "8.5.0"
+    val orgJetBrainsKotlin = "2.0.0"
 
-    id("com.android.application") version "$com_android" apply false
-    id("com.android.library") version "$com_android" apply false
+    id("com.android.application") version "$comAndroid" apply false
+    id("com.android.library") version "$comAndroid" apply false
     id("io.ktor.plugin") version "2.3.12" apply false
     id("org.gradle.android.cache-fix") version "3.0.1" apply false
-    id("org.jetbrains.kotlin.android") version "$org_jetbrains_kotlin" apply false
-    id("org.jetbrains.kotlin.jvm") version "$org_jetbrains_kotlin" apply false
-    id("org.jetbrains.kotlin.js") version "$org_jetbrains_kotlin" apply false
-    id("org.jetbrains.kotlin.multiplatform") version "$org_jetbrains_kotlin" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "$org_jetbrains_kotlin" apply false
+    id("org.jetbrains.kotlin.android") version "$orgJetBrainsKotlin" apply false
+    id("org.jetbrains.kotlin.jvm") version "$orgJetBrainsKotlin" apply false
+    id("org.jetbrains.kotlin.js") version "$orgJetBrainsKotlin" apply false
+    id("org.jetbrains.kotlin.multiplatform") version "$orgJetBrainsKotlin" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "$orgJetBrainsKotlin" apply false
 }
