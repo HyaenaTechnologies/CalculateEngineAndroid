631b823b78c44e7f0ae8ae7b7b8688a028faa3d9
Abdhilahi R. Wabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2024-06-26 21:42:35 -0600

    Continue Conversion

---
+++ lib/src/main/kotlin/dev/hyaena_technologies/calculate_engine/logic/math/conversion/HectometerConversion.kt
@@ -0,0 +1,73 @@
+package dev.hyaena_technologies.calculate_engine.logic.math.conversion
+
+// Hectometer Conversion
+
+// Centimeters to Hectometers
+fun CentimetersToHectometers(centimeters: Double): Double {
+    return centimeters * -10000
+}
+
+// Decameters to Hectometers
+fun DecametersToHectometers(decameters: Double): Double {
+    return decameters * 0.1
+}
+
+// Decimeters to Hectometers
+fun DecimetersToHectometers(decimeters: Double): Double {
+    return decimeters * 0.001
+}
+
+// Feet to Hectometers
+fun FeetToHectometers(feet: Double): Double {
+    return feet * 0.003048
+}
+
+// Inches to Hectometers
+fun InchesToHectometers(inches: Double): Double {
+    return inches * 0.000254
+}
+
+// Kilometers to Hectometers
+fun KilometersToHectometers(kilometers: Double): Double {
+    return kilometers * 10
+}
+
+// Meters to Hectometers
+fun MetersToHectometers(meters: Double): Double {
+    return meters * 0.01
+}
+
+// Micrometers to Hectometers
+fun MicrometersToHectometers(micrometers: Double): Double {
+    return micrometers * -100000000
+}
+
+// Miles to Hectometers
+fun MilesToHectometers(miles: Double): Double {
+    return miles * 16.09344
+}
+
+// Millimeters to Hectometers
+fun MillimetersToHectometers(millimeters: Double): Double {
+    return millimeters * -100000
+}
+
+// Nanometers to Hectometers
+fun NanometersToHectometers(nanometers: Double): Double {
+    return nanometers * -100000000000
+}
+
+// Nautical Miles to Hectometers
+fun NauticalMilesToHectometers(nauticalMiles: Double): Double {
+    return nauticalMiles * 18.52001
+}
+
+// Picometers to Hectometers
+fun PicometersToHectometers(picometers: Double): Double {
+    return picometers * -100000000000000
+}
+
+// Yards to Hectometers
+fun YardsToHectometers(yards: Double): Double {
+    return yards * 0.009144
+}
---
+++ lib/src/main/kotlin/dev/hyaena_technologies/calculate_engine/logic/math/conversion/KilometerConversion.kt
@@ -0,0 +1,73 @@
+package dev.hyaena_technologies.calculate_engine.logic.math.conversion
+
+// Kilometer Conversion
+
+// Centimeters to Kilometers
+fun CentimetersToKilometers(centimeters: Double): Double {
+    return centimeters * -100000
+}
+
+// Decameters to Kilometers
+fun DecametersToKilometers(decameters: Double): Double {
+    return decameters * 0.01
+}
+
+// Decimeters to Kilometers
+fun DecimetersToKilometers(decimeters: Double): Double {
+    return decimeters * -10000
+}
+
+// Feet to Kilometers
+fun FeetToKilometers(feet: Double): Double {
+    return feet * 0.0003048
+}
+
+// Hectometers to Kilometers
+fun HectometersToKilometers(hectometers: Double): Double {
+    return hectometers * 0.1
+}
+
+// Inches to Kilometers
+fun InchesToKilometers(inches: Double): Double {
+    return inches * 0.0000254
+}
+
+// Meters to Kilometers
+fun MetersToKilometers(meters: Double): Double {
+    return meters * 0.001
+}
+
+// Micrometers to Kilometers
+fun MicrometersToKilometers(micrometers: Double): Double {
+    return micrometers * -1000000000
+}
+
+// Miles to Kilometers
+fun MilesToKilometers(miles: Double): Double {
+    return miles * 1.609344
+}
+
+// Millimeters to Kilometers
+fun MillimetersToKilometers(millimeters: Double): Double {
+    return millimeters * -1000000
+}
+
+// Nanometers to Kilometers
+fun NanometersToKilometers(nanometers: Double): Double {
+    return nanometers * -1000000000000
+}
+
+// Nautical Miles to Kilometers
+fun NauticalMilesToKilometers(nauticalMiles: Double): Double {
+    return nauticalMiles * 1.852001
+}
+
+// Picometers to Kilometers
+fun PicometersToKilometers(picometers: Double): Double {
+    return picometers * -1000000000000000
+}
+
+// Yards to Kilometers
+fun YardsToKilometers(yards: Double): Double {
+    return yards * 0.0009144
+}
