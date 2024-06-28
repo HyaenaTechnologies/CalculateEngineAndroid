6af3e3c8eca310c9f8b3db849e03ccdd3e270f4c
Abdhilahi R. Wabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2024-06-28 03:11:48 -0600

    FIx Formating

--- lib/src/main/kotlin/dev/calculate_engine/logic/math/constant/Pi.kt
+++ lib/src/main/kotlin/dev/calculate_engine/logic/math/constant/Pi.kt
@@ -2,6 +2,6 @@ package dev.calculate_engine.logic.math.constant
 
 // Value of Pi
 fun pi(): Double {
-    val value: Double = 3.141592653589793
+    val value = 3.141592653589793
     return value
 }
